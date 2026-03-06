c1aec1eb905248fb01ad342acb8e6039a5d9ac1b
Jad Katerji <jadkaterjio@gmail.com>
2026-03-06 14:31:01 +0200

    Revert "Merge branch 'PID-'"
    This reverts commit 9430a186c156eab1bb9c0b2192f9f83d73c01d56, reversing
    changes made to 160a5aae795610236365e2abe8c8e963a74ecab1.

--- src/drone_library.cpp
+++ src/drone_library.cpp
@@ -3,76 +3,7 @@
 #define MAG_ADDR 0x0C
 
 
-<<<<<<< HEAD
 
-=======
-//-------------------------------------------------------------PID Controller code----------------------------------------------------------------
-
-PIDAxis::PIDAxis(float p, float i, float d) : kp(p), ki(i), kd(d) {}
-
-float PIDAxis::calculate(float target, float current, float dt) {
-    float error = target - current;
-    
-    // Apply deadband ONLY to the Proportional output to stop jitter
-    float pError = (abs(error) < 1.0) ? 0 : error; 
-
-    // Always update Integral and Derivative to keep the math smooth
-    integral += error * dt;
-    integral = constrain(integral, -iLimit, iLimit);
-    
-    float derivative = (error - lastError) / dt;
-    lastError = error;
-
-    return (kp * pError) + (ki * integral) + (kd * derivative);
-}
- 
-// Create the instances (Global to the library)
-PIDAxis pitchPID(2.0, 0.0, 0.0);
-PIDAxis rollPID(2.0, 0.00, 0.0);
-//PIDAxis yawPID(2.0, 0.0, 0.1);
-
-
-MotorSpeeds applyFlightControl(float targetP, float targetR, float targetY, int throttle, bool debug) {
-    // throttle parsed as 0-255
-    // targetP (Pitch), targetR (Roll), targetY (Yaw) are desired angles in degrees
-
-    // 1. Get IMU Data (assuming degrees)
-    DroneSensors imu = readSensors();
-    float dt = 0.004; // 4ms loop time
-
-    // 2. Calculate PID outputs for each axis
-    float pitchAdj = pitchPID.calculate(targetP, imu.smoothedpitch, dt);
-    float rollAdj  = rollPID.calculate(targetR, imu.smoothedroll, dt);
-    //float yawAdj   = yawPID.calculate(targetY, imu.yaw, dt);
-
-    // 3. Motor Mixing (X-Configuration)
-    int mTL = throttle + pitchAdj + rollAdj ;
-    int mTR = throttle + pitchAdj - rollAdj ;
-    int mBL = throttle - pitchAdj + rollAdj ;
-    int mBR = throttle - pitchAdj - rollAdj ;
-
-    // Constrain motor outputs to 0-255 for safety
-    MotorSpeeds speeds = {
-        map(constrain(mTL, 0, 255), 0, 255, 0, 100),
-        map(constrain(mTR, 0, 255), 0, 255, 0, 100),
-        map(constrain(mBL, 0, 255), 0, 255, 0, 100),
-        map(constrain(mBR, 0, 255), 0, 255, 0, 100)
-    };
-
-    // 4. Apply
-    applyMotorPower(speeds.tl, speeds.tr, speeds.bl, speeds.br);
-
-    if (debug) {
-        delay(500); // Slow down for readability
-        Serial.println();
-        Serial.print("Pitch:"); Serial.print(imu.smoothedpitch); Serial.print(" | Roll:"); Serial.print(imu.smoothedroll);
-        Serial.print(" | Adj P:"); Serial.print(pitchAdj); Serial.print(" | Adj R:"); Serial.print(rollAdj);
-        Serial.print(" | Motors (TL, TR, BL, BR): "); Serial.print(speeds.tl); Serial.print(", ");Serial.print(speeds.tr); Serial.print(", ");Serial.print(speeds.bl); Serial.print(", ");Serial.println(speeds.br);
-    } 
-
-    return speeds;
-}
->>>>>>> PID-
 //------------------------------------------------------------Communication code-------------------------------------------------------------
 const char* html_control_page = R"=====(
 <!DOCTYPE html>
@@ -87,7 +18,7 @@ const char* html_control_page = R"=====(
     </style>
 </head>
 <body>
-    <h2>ESP32 DRONE CONTROL </h2>
+    <h2>ESP32 DRONE PROTOTYPE</h2>
     <p>Throttle: <span id="tVal">0</span>%</p>
     <input type="range" id="throttle" class="slider" min="0" max="100" value="0">
     <div id="joystick"><div id="knob"></div></div>
@@ -170,7 +101,6 @@ void initMotors() {
 }
 
 void applyMotorPower(int tl, int tr, int bl, int br) {
-    //input is 0-100% for each motor
     // 50Hz means 20ms total period.
     // 1ms pulse (OFF) = 5% of 20ms = 3277 (in 16-bit)
     // 2ms pulse (FULL) = 10% of 20ms = 6554 (in 16-bit)
@@ -186,10 +116,6 @@ void applyMotorPower(int tl, int tr, int bl, int br) {
     ledcWrite(3, percentToDuty(br));
 }
 
-
-    
-
-
 //-----------------------------------------------------------Sensor code-------------------------------------------------------------------
 
 Adafruit_MPU6050 mpu;
@@ -199,39 +125,23 @@ bool initSensors(int sda, int scl) {
     
     if (!mpu.begin()) return false;
 
+    
     delay(100);
     return true; 
 }
 
 DroneSensors readSensors() {
-
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
 
     DroneSensors data;
-    // Store Raw
     data.accX = a.acceleration.x;
     data.accY = a.acceleration.y;
     data.accZ = a.acceleration.z;
     data.gyroX = g.gyro.x;
     data.gyroY = g.gyro.y;
     data.gyroZ = g.gyro.z;
-
-    static float lastroll = 0;
-    static float lastpitch = 0;
-    // Calculate Degrees (The "Angles and Stuff")
-    // Roll: rotation around X-axis
-    data.smoothedroll = 0.7 * lastroll + 0.3 * (atan2(data.accY, data.accZ) * 180 / PI + 180); // Add 180 to make roll back to 0 since the imu sensor will be flipped upside down on the drone. 
-    data.smoothedpitch = 0.7 * lastpitch + 0.3 * (atan2(-data.accX, sqrt(data.accY * data.accY + data.accZ * data.accZ)) * 180 / PI);
     
-<<<<<<< HEAD
     
-=======
-    // Store last values for smoothing
-    lastpitch = data.smoothedpitch;
-    lastroll = data.smoothedroll;
-
->>>>>>> PID-
     return data;
-   
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,15 @@ void setup() {
     delay(1000); // Give the serial monitor time to connect
     initWiFi();
     initMotors();
-    initSensors(4, 5);
+    
     Serial.println("Setup Complete.");
-<<<<<<< HEAD
     
 
-=======
-   
->>>>>>> PID-
     
 }
 
 void loop() {
     handleComm(); // Handle incoming WebSocket commands
     DroneCommands commands = getRemoteCommands();
-<<<<<<< HEAD
-=======
-
-    DroneSensors imu = readSensors();
-
-    MotorSpeeds current_speeds = applyFlightControl(0.0,0.0,0.0, 50);
-
->>>>>>> PID-
     
 }
